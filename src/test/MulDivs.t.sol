1f211013bad10619cd87af112ba5d18d6cfa34ff
t11s <greenfeatherhelp@gmail.com>
2021-11-28 16:35:13 -0800

    fix: use test values that avoid early exit

--- src/test/MulDivs.t.sol
+++ src/test/MulDivs.t.sol
@@ -16,11 +16,23 @@ contract MulDivTests is DSTestPlus {
         assertEq(mulDivs.remcoMulDiv(5e18, 0.1e18, 1e18), 0.5e18);
     }
 
+    function testRemcoBig() public {
+        assertEq(mulDivs.remcoMulDiv(5000e45, 0.1e45, 1e45), 500e45);
+    }
+
     function testMikhail() public {
         assertEq(mulDivs.mikhailMulDiv(5e18, 0.1e18, 1e18), 0.5e18);
     }
 
+    function testMikhailBig() public {
+        assertEq(mulDivs.mikhailMulDiv(5000e45, 0.1e45, 1e45), 500e45);
+    }
+
     function testMikhailInlinedFullMul() public {
         assertEq(mulDivs.mikhailMulDivInlinedFullMul(5e18, 0.1e18, 1e18), 0.5e18);
+    }
+
+    function testMikhailInlinedFullMulBig() public {
+        assertEq(mulDivs.mikhailMulDivInlinedFullMul(5000e45, 0.1e45, 1e45), 500e45);
     }
 }
